4ae4388c85462ec27ab2deeb2319ea7f8c2409f3
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-10-29 20:03:41 +0100

    fix(general-settings.component): loading photo on front-end

--- src/app/features/settings/school/general-settings.component.ts
+++ src/app/features/settings/school/general-settings.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-general-settings',
   templateUrl: './general-settings.component.html',
-  styleUrl: './general-settings.component.css'
+  styleUrls: ['./general-settings.component.css']
 })
 export class GeneralSettingsComponent {
+  @ViewChild('fileInput') fileInput!: ElementRef;
 
+  // Метод для открытия диалога выбора файла
+  triggerFileUpload() {
+    this.fileInput.nativeElement.click();
+  }
+
+  // Метод, который вызывается при выборе файла
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      console.log('Файл выбран:', file);
+
+      // Здесь можно добавить логику загрузки файла на сервер
+      // Например, передача файла на backend или обработка его на клиенте
+    }
+  }
 }
