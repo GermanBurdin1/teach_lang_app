40ca82f524968a7dd1274a6dffe7919584531c60
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-10-30 20:48:03 +0100

    feat(access-control): access control modal

--- src/app/features/settings/school/access-control.component.ts
+++ src/app/features/settings/school/access-control.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-access-control',
   templateUrl: './access-control.component.html',
-  styleUrl: './access-control.component.css'
+  styleUrls: ['./access-control.component.css']
 })
 export class AccessControlComponent {
+  showModal: boolean = false;
+  activeTab: string = 'teachers';  // Изначально открыта вкладка "Учителя"
 
+  openCreateGroupModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  selectTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
