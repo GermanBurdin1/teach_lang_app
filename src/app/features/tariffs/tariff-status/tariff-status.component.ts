aeaebc9459d2d901eba0da75de37756e49dba854
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-11-01 11:49:30 +0100

    style(tariffs): main component

--- src/app/features/tariffs/tariff-status/tariff-status.component.ts
+++ src/app/features/tariffs/tariff-status/tariff-status.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tariff-status',
   templateUrl: './tariff-status.component.html',
+  styleUrl: './tariff-status.component.css'
 })
 export class TariffStatusComponent {
   showAllFeatures() {
