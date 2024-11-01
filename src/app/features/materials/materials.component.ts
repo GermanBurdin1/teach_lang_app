7bb96cad4e189649ecd5d51c3450465415cdc8c5
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-11-01 15:20:59 +0100

    feat: materials

--- src/app/features/materials/materials.component.ts
+++ src/app/features/materials/materials.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-materials',
   templateUrl: './materials.component.html',
-  styleUrl: './materials.component.css'
+  styleUrls: ['./materials.component.css']
 })
 export class MaterialsComponent {
+  selectedFile: File | null = null;
 
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      console.log('Выбранный файл:', this.selectedFile.name);
+    }
+  }
 }
