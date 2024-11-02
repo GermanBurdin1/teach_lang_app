66de81e546e21502464184fa77e10ca1e35e313d
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-11-02 14:03:38 +0100

    feat(users): select for timezone and courses frequency

--- src/app/features/users/users.component.ts
+++ src/app/features/users/users.component.ts
@@ -9,6 +9,19 @@ export class UsersComponent {
   isCreateStudentModalOpen = false;
   showAdditionalInfo = false;
 
+  // Timezone options (all UTC)
+  timezones = [
+    'UTC-12:00', 'UTC-11:00', 'UTC-10:00', 'UTC-09:00', 'UTC-08:00', 'UTC-07:00', 'UTC-06:00', 'UTC-05:00', 'UTC-04:00',
+    'UTC-03:00', 'UTC-02:00', 'UTC-01:00', 'UTC+00:00', 'UTC+01:00', 'UTC+02:00', 'UTC+03:00', 'UTC+04:00',
+    'UTC+05:00', 'UTC+06:00', 'UTC+07:00', 'UTC+08:00', 'UTC+09:00', 'UTC+10:00', 'UTC+11:00', 'UTC+12:00'
+  ];
+
+  // Frequency options for "Периодичность обучения"
+  frequencies = [
+    '1 раз в неделю', '2 раза в неделю', '3 раза в неделю', '4 раза в неделю',
+    '5 раз в неделю', '6 раз в неделю', '7 раз в неделю'
+  ];
+
   openCreateStudentModal(): void {
     this.isCreateStudentModalOpen = true;
   }
@@ -24,5 +37,3 @@ export class UsersComponent {
     this.showAdditionalInfo = !this.showAdditionalInfo;
   }
 }
-
-
