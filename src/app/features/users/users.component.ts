61be65471157b5e3a5a497fb99c1d561d4882683
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-11-02 12:45:10 +0100

    feat(user): first version users component

--- src/app/features/users/users.component.ts
+++ src/app/features/users/users.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  isCreateStudentModalOpen = false;
+  showAdditionalInfo = false;
 
+  openCreateStudentModal(): void {
+    this.isCreateStudentModalOpen = true;
+  }
+
+  closeCreateStudentModal(event?: MouseEvent): void {
+    if (event) {
+      event.stopPropagation();
+    }
+    this.isCreateStudentModalOpen = false;
+  }
+
+  toggleAdditionalInfo(): void {
+    this.showAdditionalInfo = !this.showAdditionalInfo;
+  }
 }
+
+
