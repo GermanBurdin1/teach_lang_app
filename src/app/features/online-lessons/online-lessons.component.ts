1ae2ab4bf07f4980ba66ff434c57e7dc40747373
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-11-10 16:02:25 +0100

    feat(new component): online-lessons

---
+++ src/app/features/online-lessons/online-lessons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-online-lessons',
+  templateUrl: './online-lessons.component.html',
+  styleUrl: './online-lessons.component.css'
+})
+export class OnlineLessonsComponent {
+
+}
