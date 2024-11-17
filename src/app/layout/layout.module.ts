785293ac8a5d34cc9159a587ed10d66253d7eec3
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-11-17 17:53:05 +0100

    feat(header): header

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent
   ]
 })
 export class LayoutModule { }
