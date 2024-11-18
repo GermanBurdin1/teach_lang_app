74b23e40e75f9c31d1c9151f8c3a8f3708a687d3
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-11-18 10:18:00 +0100

    feat(header): модалка пополнения суммы

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -21,4 +21,15 @@ export class HeaderComponent {
     // Логика для переключения на роль ученика
     alert('Переключение на ученика');
   }
+
+  // ajouter de l'argent
+  showBalanceModal = false;
+
+  openBalanceModal(): void {
+    this.showBalanceModal = true;
+  }
+
+  closeBalanceModal(): void {
+    this.showBalanceModal = false;
+  }
 }
