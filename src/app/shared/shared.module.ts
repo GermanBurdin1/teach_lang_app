200960aaa924bb56b847146f3bab3336f9621867
German <125369843+GermanBurdin1@users.noreply.github.com>
2025-09-12 22:17:38 +0200

    Enhance Shared Module with Additional Angular Material Components
    - Added new Angular Material modules: MatCardModule, MatButtonToggleModule, MatFormFieldModule, MatSelectModule, MatOptionModule, and MatRadioModule to the SharedModule.
    - Included FormsModule in imports and exports for improved form handling capabilities.
    - Updated the declarations and exports to ensure all components and modules are properly included.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,23 +8,47 @@ import { CalendarPreviewComponent } from './components/calendar/calendar-preview
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ScheduleComponent, CalendarPreviewComponent],
   imports: [
     CommonModule,
+    FormsModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
     }),
     MatButtonModule,
     MatIconModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatRadioModule
   ],
   exports: [
     ScheduleComponent,
     CalendarPreviewComponent,
-    MatSnackBarModule
+    CommonModule,
+    FormsModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatRadioModule
   ]
 })
 export class SharedModule { }
